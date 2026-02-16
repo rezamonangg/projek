5c39d7b6c01085e6ae2f139dbd5dde5603c0a535
Monang <rezadhah@protonmail.com>
2026-02-16 12:12:23 +0700

    feat: add api index with all mock exports

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -1,1 +1,12 @@
-export {};
+export * as authApi from './mock/auth';
+export * as membersApi from './mock/members';
+export * as projectsApi from './mock/projects';
+export * as epicsApi from './mock/epics';
+export * as boardsApi from './mock/boards';
+export * as tasksApi from './mock/tasks';
+export * as labelsApi from './mock/labels';
+export * as wikiApi from './mock/wiki';
+export * as filesApi from './mock/files';
+export * as adminApi from './mock/admin';
+
+export * from './types';
