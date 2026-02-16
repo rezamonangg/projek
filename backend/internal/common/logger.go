7cad5b27fefa87f2924659c5570e4cb5762fdf8a
Monang <rezadhah@protonmail.com>
2026-02-16 16:45:53 +0700

    fix: output logs as JSON instead of human-readable format

--- backend/internal/common/logger.go
+++ backend/internal/common/logger.go
@@ -2,14 +2,12 @@ package common
 
 import (
 	"os"
-	"time"
 
 	"github.com/rs/zerolog"
 )
 
 func NewLogger(cfg *Config) zerolog.Logger {
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	logger := zerolog.New(output).With().Timestamp().Str("appname", "projek").Logger()
+	logger := zerolog.New(os.Stdout).With().Timestamp().Str("appname", "projek").Logger()
 	logger.Level(zerolog.InfoLevel)
 	return logger
 }
