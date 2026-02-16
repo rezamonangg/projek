2fac79aafb1ea554ec774809e2a1405760b6e306
Monang <rezadhah@protonmail.com>
2026-02-16 12:19:07 +0700

    feat: add auth and app layout components

---
+++ frontend/src/routes/(app)/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { MainLayout } from '$lib/components/layout';
+	import { ToastContainer, setToastContext } from '$lib/components/common';
+
+	let { children } = $props();
+</script>
+
+<MainLayout>
+	{@render children()}
+</MainLayout>
+
+<ToastContainer />
